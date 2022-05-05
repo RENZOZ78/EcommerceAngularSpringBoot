d3c1d98e3756a98b9074b8d11351736ab884774d
RENZOZ78 <didierbap@gmail.com>
2022-05-05 19:42:27 +0200

    sauvegarde de commande dans la bdd-backend

---
+++ 02-backend/spring-boot-ecommerce/src/main/java/com/luv2code/ecommerce/controller/CheckoutController.java
@@ -0,0 +1,28 @@
+package com.luv2code.ecommerce.controller;
+
+import com.luv2code.ecommerce.dto.Purchase;
+import com.luv2code.ecommerce.dto.PurchaseResponse;
+import com.luv2code.ecommerce.service.CheckoutService;
+import org.springframework.web.bind.annotation.*;
+
+@CrossOrigin(value = "http://localhost:4200", exposedHeaders = "Access-Control-Allow-Origin")
+@RestController
+@RequestMapping("/api/checkout")
+public class CheckoutController {
+    private CheckoutService checkoutService;
+
+
+    public CheckoutController(CheckoutService checkoutService){
+        this.checkoutService = checkoutService;
+    }
+
+    // methode qui renvoie une response d'achat
+    @PostMapping("/purchase")
+    public  PurchaseResponse placeOrder(@RequestBody Purchase purchase) {
+        PurchaseResponse purchaseResponse = checkoutService.placeOrder(purchase);
+
+        return purchaseResponse;
+    }
+
+
+}
---
+++ 02-backend/spring-boot-ecommerce/src/main/java/com/luv2code/ecommerce/service/CheckoutServiceImpl.java
@@ -0,0 +1,63 @@
+package com.luv2code.ecommerce.service;
+
+import com.luv2code.ecommerce.dao.CustomerRepository;
+import com.luv2code.ecommerce.dto.Purchase;
+import com.luv2code.ecommerce.dto.PurchaseResponse;
+import com.luv2code.ecommerce.entity.Customer;
+import com.luv2code.ecommerce.entity.Order;
+import com.luv2code.ecommerce.entity.OrderItem;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+import java.util.Set;
+import java.util.UUID;
+
+@Service
+public class CheckoutServiceImpl implements  CheckoutService {
+
+    private CustomerRepository customerRepository;
+
+    @Autowired
+    public  CheckoutServiceImpl(CustomerRepository customerRepository){
+        this.customerRepository = customerRepository;
+    }
+
+    @Override
+    @Transactional
+    public PurchaseResponse placeOrder(Purchase purchase) {
+
+        //recupere la commande information a partir du dto
+        Order order = purchase.getOrder();
+
+        //genere un numero de suivi- de commande
+        String orderTrackingNumber = generateOrderTrackingNUmber();
+        order.setOrderTrackingNumber(orderTrackingNumber);
+
+        //rempli les commande avec les orderItems
+        Set<OrderItem> orderItems = purchase.getOrderItems();
+        orderItems.forEach(item-> order.add(item));
+
+        //rempli la commande avec billingaddress et shippingaddress
+        order.setBillingAddress(purchase.getBillingAddress());
+        order.setShippingAddress(purchase.getShippingAddress());
+
+        //rempli les customeres avec l'order
+        Customer customer = purchase.getCustomer();
+        customer.add(order);
+
+        //savegarde dans le database
+          customerRepository.save(customer);
+
+        //return avec une response
+        return new PurchaseResponse(orderTrackingNumber);
+
+    }
+
+    private String generateOrderTrackingNUmber() {
+
+        //generer un UUID aleatoire
+        return UUID.randomUUID().toString();
+        
+    }
+}
