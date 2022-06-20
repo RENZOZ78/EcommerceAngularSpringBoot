b373a6f4796e77a45e44f99cf9082ee6ba4f2215
RENZOZ78 <didierbap@gmail.com>
2022-06-20 16:21:40 +0200

    Version 3.0 - security - order history - front (coder orderHisotry service)

--- 03-frontend/angular-ecommerce/src/app/services/order-history.service.ts
+++ 03-frontend/angular-ecommerce/src/app/services/order-history.service.ts
@@ -1,9 +1,31 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { OrderHistory } from '../common/order-history';
 
 @Injectable({
   providedIn: 'root'
 })
 export class OrderHistoryService {
 
-  constructor() { }
+  private orderUrl = 'http://localhost:8080/api/orders';
+
+  constructor(private httpClient: HttpClient) { }
+
+  getOrderHistory(theEmail: string): Observable<GetResponseOrderHistory>{
+
+    //besoin de construire un url basé sur l'email du client
+    const orderHistoryUrl = `${this.orderUrl}/search/findByCustomerEmail?email=${theEmail}`;
+
+    return this.httpClient.get<GetResponseOrderHistory>(orderHistoryUrl);
+
+  }
+
+
+}
+
+interface GetResponseOrderHistory{
+  _embedded: {
+    orders: OrderHistory[];
+  }
 }
