7c60fdf12076ed5d0011f05754372d79bcd7d457
RENZOZ78 <didierbap@gmail.com>
2022-06-21 17:04:52 +0200

    Version 3.0 - security - order history - securiser back (protection du endpoints)

---
+++ 02-backend/spring-boot-ecommerce/src/main/java/com/luv2code/ecommerce/config/SecurityConfiguration.java
@@ -0,0 +1,25 @@
+package com.luv2code.ecommerce.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        super.configure(http);
+
+        //proteger point de terminaison  /api/orders
+        http.authorizeRequests()
+                .antMatchers("/api/orders/**")
+                .authenticated()
+                .and()
+                .oauth2ResourceServer()
+                .jwt();
+
+        //add CORS filters
+        http.cors();
+
+    }
+}
