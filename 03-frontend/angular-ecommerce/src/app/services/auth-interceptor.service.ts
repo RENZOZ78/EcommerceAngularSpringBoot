77196e0e6ddf93ab32dfd4cff56757d589829688
RENZOZ78 <didierbap@gmail.com>
2022-06-24 12:02:20 +0200

    Version 3.0 - security - order history - securiser front (mettre en place le pass token)

--- 03-frontend/angular-ecommerce/src/app/services/auth-interceptor.service.ts
+++ 03-frontend/angular-ecommerce/src/app/services/auth-interceptor.service.ts
@@ -1,5 +1,7 @@
 import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { OktaAuthStateService } from '@okta/okta-angular';
+import { OktaAuth } from '@okta/okta-auth-js';
 import { format } from 'path';
 import { from, Observable } from 'rxjs';
 
@@ -8,12 +10,30 @@ import { from, Observable } from 'rxjs';
 })
 export class AuthInterceptorService implements HttpInterceptor {
 
-  constructor() { }
+  constructor(private oktaAuth: OktaAuth) { }
   intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-   return from(this.handleAcess(request, next));
-    
+   return from(this.handleAcess(request, next));    
   }
+
   private async handleAcess(request: HttpRequest<any>, next: HttpHandler): Promise<HttpEvent<any>> {
-    throw new Error('Method not implemented.');
+   
+    //rajouter  un access token pour un point securisé
+    const securedEndpoints = ['htp://localhost:8080/api/orders'];
+
+    if (securedEndpoints.some(url => request.urlWithParams.includes(url))) {
+
+      //Recuperer l'access token
+      const accessToken = await this.oktaAuth.getAccessToken();
+
+      //copier la requete et ajouter la nouvelle entete avec le jeton
+      request = request.clone({
+        setHeaders: {
+          Authorization: 'Bearer ' + accessToken,
+        }
+      });
+    }
+
+    return next.handle(request).toPromise();
+
   }
 }
