9ac5e252050a4a098a3a5783ea080aa355b87af6
RENZOZ78 <didierbap@gmail.com>
2022-07-04 12:18:48 +0200

    Version 3.0 - security - Sécuriser la communication avec HTTPS - frontend - configuration okta (redirect URI + okta dashboard)

--- 03-frontend/angular-ecommerce/src/app/config/my-app-config.ts
+++ 03-frontend/angular-ecommerce/src/app/config/my-app-config.ts
@@ -5,7 +5,7 @@ export default{
     iodc:{
         clientId: '0oa5793ke0KPU0dlR5d7',
         issuer: 'https://dev-63517545.okta.com/oauth2/default',
-        redirectUri: 'http://localhost:4200/login/callback',
+        redirectUri: 'https://localhost:4200/login/callback',
 
         //etendue
             // openid: requiert requete authentification
