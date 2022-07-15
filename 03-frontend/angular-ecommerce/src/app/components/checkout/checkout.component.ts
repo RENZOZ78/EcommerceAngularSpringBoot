a985064bf2bacc91152affbfbd8859bd782270b7
RENZOZ78 <didierbap@gmail.com>
2022-07-15 16:17:03 +0200

    Processus de Paiment - Stripe - ajout details clients + description paiement stripe

--- 03-frontend/angular-ecommerce/src/app/components/checkout/checkout.component.ts
+++ 03-frontend/angular-ecommerce/src/app/components/checkout/checkout.component.ts
@@ -341,7 +341,19 @@ export class CheckoutComponent implements OnInit {
           this.stripe.confirmCardPayment(payementIntentResponse.client_secret,
             {
               payment_method: {
-                card: this.cardElement
+                card: this.cardElement,
+
+                billing_details: {
+                  email: purchase.customer.email,
+                  name: `${purchase.customer.firstName} ${purchase.customer.lastName}`,
+                  address: {
+                    line1: purchase.billingAddress.street,
+                    city: purchase.billingAddress.city,
+                    state: purchase.billingAddress.state,
+                    postal_code: purchase.billingAddress.zipCode,
+                    country: this.billingAddressCountry.value.code
+                  }
+                }
               }
             }, {handleActions: false})
           .then(function(result){
